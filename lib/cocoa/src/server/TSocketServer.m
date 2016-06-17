f4567f030fb420c240584df37629eed2e87faaed
Chris Vasselli <cvasselli@box.com>
2016-06-17 13:41:31 -0400

    Add support for Unix Domain Sockets to TSocketServer and TSocketClient.

--- lib/cocoa/src/server/TSocketServer.m
+++ lib/cocoa/src/server/TSocketServer.m
@@ -25,6 +25,7 @@
 #import "TObjective-C.h"
 #import <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/un.h>
 
 
 
@@ -32,6 +33,12 @@ NSString * const kTSocketServer_ClientConnectionFinishedForProcessorNotification
 NSString * const kTSocketServer_ProcessorKey = @"TSocketServer_Processor";
 NSString * const kTSockerServer_TransportKey = @"TSockerServer_Transport";
 
+@interface TSocketServer ()
+{
+    NSString *mDomainSocketPath;
+}
+
+@end
 
 @implementation TSocketServer
 
@@ -39,20 +46,25 @@ NSString * const kTSockerServer_TransportKey = @"TSockerServer_Transport";
     protocolFactory: (id <TProtocolFactory>) protocolFactory
    processorFactory: (id <TProcessorFactory>) processorFactory
 {
-  self = [super init];
+  CFSocketRef socket = [self createSocketWithPort:port];
+  if (socket == NULL) {
+      return nil;
+  }
 
-  mInputProtocolFactory = [protocolFactory retain_stub];
-  mOutputProtocolFactory = [protocolFactory retain_stub];
-  mProcessorFactory = [processorFactory retain_stub];
+  if (self = [self initWithSocket:socket protocolFactory:protocolFactory processorFactory:processorFactory]) {
+      NSLog(@"Listening on TCP port %d", port);
+  }
+  return self;
+}
 
-  // create a socket.
-  int fd = -1;
+- (CFSocketRef) createSocketWithPort: (int) port
+{
   CFSocketRef socket = CFSocketCreate(kCFAllocatorDefault, PF_INET, SOCK_STREAM, IPPROTO_TCP, 0, NULL, NULL);
   if (socket) {
     CFOptionFlags flagsToClear = kCFSocketCloseOnInvalidate;
     CFSocketSetSocketFlags(socket,  CFSocketGetSocketFlags(socket) & ~flagsToClear);
 
-    fd = CFSocketGetNative(socket);
+    int fd = CFSocketGetNative(socket);
     int yes = 1;
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (void *)&yes, sizeof(yes));
 
@@ -67,13 +79,29 @@ NSString * const kTSockerServer_TransportKey = @"TSockerServer_Transport";
       CFSocketInvalidate(socket);
       CFRelease(socket);
       NSLog(@"*** Could not bind to address");
-      return nil;
+      return NULL;
     }
+
+    return socket;
   } else {
     NSLog(@"*** No server socket");
-    return nil;
+    return NULL;
   }
-  
+}
+
+- (id) initWithSocket: (CFSocketRef) socket
+      protocolFactory: (id <TProtocolFactory>) protocolFactory
+     processorFactory: (id <TProcessorFactory>) processorFactory
+{
+  self = [super init];
+
+  mInputProtocolFactory = [protocolFactory retain_stub];
+  mOutputProtocolFactory = [protocolFactory retain_stub];
+  mProcessorFactory = [processorFactory retain_stub];
+
+  // create a socket.
+  int fd = CFSocketGetNative(socket);
+
   // wrap it in a file handle so we can get messages from it
   mSocketFileHandle = [[NSFileHandle alloc] initWithFileDescriptor: fd
                                                     closeOnDealloc: YES];
@@ -91,11 +119,64 @@ NSString * const kTSockerServer_TransportKey = @"TSockerServer_Transport";
   // tell socket to listen
   [mSocketFileHandle acceptConnectionInBackgroundAndNotify];
   
-  NSLog(@"Listening on TCP port %d", port);
-  
   return self;
 }
 
+- (id) initWithPath: (NSString *) path
+    protocolFactory: (id <TProtocolFactory>) protocolFactory
+   processorFactory: (id <TProcessorFactory>) processorFactory
+{
+  mDomainSocketPath = path;
+  CFSocketRef socket = [self createSocketWithPath:path];
+  if (socket == NULL) {
+      return nil;
+  }
+
+  if (self = [self initWithSocket:socket protocolFactory:protocolFactory processorFactory:processorFactory]) {
+      NSLog(@"Listening on path %@", path);
+  }
+  return self;
+}
+
+- (CFSocketRef) createSocketWithPath: (NSString *) path
+{
+  CFSocketRef socket = CFSocketCreate(kCFAllocatorDefault, PF_LOCAL, SOCK_STREAM, IPPROTO_IP, 0, NULL, NULL);
+  if (socket) {
+    CFOptionFlags flagsToClear = kCFSocketCloseOnInvalidate;
+    CFSocketSetSocketFlags(socket,  CFSocketGetSocketFlags(socket) & ~flagsToClear);
+
+    int fd = CFSocketGetNative(socket);
+    int yes = 1;
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (void *)&yes, sizeof(yes)) < 0) {
+        NSLog(@"*** Unable to set REUSEADDR on socket.");
+        return NULL;
+    }
+
+    size_t nullTerminatedPathLength = path.length + 1;
+    struct sockaddr_un addr;
+    if (nullTerminatedPathLength> sizeof(addr.sun_path)) {
+        NSLog(@"*** Unable to create socket at path %@. Path is too long.", path);
+        return NULL;
+    }
+
+    addr.sun_family = AF_LOCAL;
+    memcpy(addr.sun_path, path.UTF8String, nullTerminatedPathLength);
+    addr.sun_len = SUN_LEN(&addr);
+
+    NSData *address = [NSData dataWithBytes:&addr length:sizeof(addr)];
+    if (CFSocketSetAddress(socket, (bridge_stub CFDataRef)address) != kCFSocketSuccess) {
+        CFSocketInvalidate(socket);
+        CFRelease(socket);
+        NSLog(@"*** Could not bind to address");
+        return NULL;
+    }
+    
+    return socket;
+  } else {
+    NSLog(@"*** No server socket");
+    return NULL;
+  }
+}
 
 - (void) dealloc {
   [[NSNotificationCenter defaultCenter] removeObserver:self];
@@ -103,6 +184,11 @@ NSString * const kTSockerServer_TransportKey = @"TSockerServer_Transport";
   [mOutputProtocolFactory release_stub];
   [mProcessorFactory release_stub];
   [mSocketFileHandle release_stub];
+
+  if (mDomainSocketPath != nil) {
+    unlink(mDomainSocketPath.UTF8String);
+  }
+
   [super dealloc_stub];
 }
 
